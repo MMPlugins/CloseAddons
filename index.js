243bb32448eb200244fc7db1d32bb945d135f266
Eight8 <66164881+Eight8-7020@users.noreply.github.com>
2020-10-27 20:29:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-module.exports = function({commands}){
-  commands.addGlobalCommand ('web', '', (msg, args) => {
+module.exports = function({bot, commands}){
+  commands.addGlobalCommand ('help', '', (msg, args) => {
   bot.createMessage (msg.channel.id, `**See the bot's commands at:**
 https://github.com/Dragory/modmailbot/blob/master/docs/commands.md`);
   });
