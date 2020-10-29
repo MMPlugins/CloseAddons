fc7f9f0b0a9533ff7fadcecf93e565243dba4b3c
Eight8 <66164881+Eight8-7020@users.noreply.github.com>
2020-10-29 11:00:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ module.exports = function({ bot, utils, commands }) {
       }
   
       const threadchannel = await thread.getDMChannel();
-      bot.createMessage(threadchannel.id, `**${args.text}**`, msg.attachments, false);
+      bot.createMessage(threadchannel.id, `${args.text}`, msg.attachments, false);
+      msg.delete();
+      thread.close();
+    });
+    commands.addInboxThreadCommand('reasonclose', '[text$]',  async (msg, args, thread) => {
+      if (! args.text && msg.attachments.length  === 0) {
+        utils.postError(msg.channel.id, 'Text or Attachment is required');
+        return;
+      }
+  
+      const threadchanneltwo = await thread.getDMChannel();
+      bot.createMessage(threadchanneltwo.id, `Reason for thread closure: ${args.text}`, msg.attachments, false);
       msg.delete();
       thread.close();
     });
